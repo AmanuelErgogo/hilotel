5eb207a2ba0cc4753f15aa1f31f6163f729d5243
AmanuelErgogo <amanuel.ergogo@gmail.com>
2023-12-12 20:43:47 +0100

    dump_json

--- play_hilotel.py
+++ play_hilotel.py
@@ -1,5 +1,9 @@
 import rospy
 import argparse
+import json
+import os
+import time
+
 
 # add argparse arguments
 parser = argparse.ArgumentParser("Welcome to hilotel: vr based human in the loop learning and execution platform!")
@@ -20,10 +24,59 @@ import robomimic.utils.torch_utils as TorchUtils
 
 from xarm7_env import XArm7Env
 
+expert_actions = [0.0, 0.0, 0.0, 0.0]
+is_interrupt = False
+
+action_type = "absolute" 
+stop = 0
+is_update = False
+done_trigger = 0
+action = [0.0, 0.0, 0.0, 0.0]
+previous_command = np.array([0.0, 0.0, 0.0])
+
+hilotel_execution_time = {}
+
+def joy_cmd_clb(msg):
+    global expert_actions, is_interrupt
+    """
+    R_trigger: open/close gripper 
+    R_B: Stop
+    R_A: Done
+    """
+    expert_actions[3] = msg.axes[2]
+    # self.done_trigger = 1 if msg.buttons[2] == 1 else 0
+    if msg.buttons[3] == 1:
+        stop = 1
+    if msg.buttons[0] == 1:
+        is_interrupt = True
+
+def oculus_delta_pose_cb(msg):
+    """Obtains expert action from oculus quest 2 interface."""
+    global action_type, previous_command, is_update
+    r_controller_pos = [msg.pos_z, -msg.pos_x, msg.pos_y]
+    if action_type == "relative":
+        """action = delta_pos between -1 and 1
+        - calculate delta 
+        - normalize between -1 and 1  to make it similar with agents action
+        """
+        if not is_update: # first command
+            previous_command = np.array(r_controller_pos).copy()
+        delta_command = np.array(r_controller_pos) - previous_command
+        normalized_delta_command = np.clip(delta_command, -1.0, 1.0)
+        previous_command = np.array(r_controller_pos).copy()
+        expert_actions[0] = normalized_delta_command[0]
+        expert_actions[1] = normalized_delta_command[1]
+        expert_actions[2] = normalized_delta_command[2]
+    else: 
+        expert_actions[0] = msg.pos_z
+        expert_actions[1] = -msg.pos_x
+        expert_actions[2] = msg.pos_y
+    is_update = True
 
 def main():
+    global expert_actions, is_interrupt
     """Run a trained policy from robomimic with ufactory xarm7 environment."""
-    rospy.init_node('hilotel_offline_policy_node', anonymous=True)
+    rospy.init_node('hilotel_eval_node', anonymous=True)
 
     rate = rospy.Rate(50)
 
@@ -49,12 +102,41 @@ def main():
         print("obs:", obs)
         print("info:", info)
     while not rospy.is_shutdown():
+        if is_new_demo:
+            user_input = input("Enter to continue next eval: Any key")
+            is_new_demo = False
+            demo_count = demo_count + 1
+            t_start = time.monotonic()
         # compute actions
-        actions = policy(obs)
+        if is_interrupt:
+            actions = policy(obs)
+        else:
+            actions = expert_actions
         print("actions:", actions)
         print("obs: ", obs)
         # apply actions
         obs, reward, terminated, truncated, info= env.step(actions)
+
+        dones = np.array([info["is_success"]])
+        
+        if dones[0] == True:
+            hilotel_execution_time[f"demo_{demo_count}_min"] = (time.monotonic() - t_start) / 60
+            is_new_demo = True
+            
+            log_dir_exec_time = os.path.join("./logs/robomimic", args_cli.task, 'agent_execution_time.json')
+            with open(log_dir_exec_time, 'w') as json_file:
+                json.dump(hilotel_execution_time, json_file)
+
+            # reset env to start second evaluation
+            max_executions = 300
+            counter = 0
+            while counter < max_executions:
+                print ("resseting>>>")
+                env.step(action=actions)
+                counter += 1
+                if counter >= max_executions:
+                    info["is_success"] = False
+                    break
 
         rate.sleep()
 
--- play_offline_policy.py
+++ play_offline_policy.py
@@ -1,5 +1,8 @@
 import rospy
 import argparse
+import os
+import time
+import json
 
 # add argparse arguments
 parser = argparse.ArgumentParser("Welcome to hilotel: vr based human in the loop learning and execution platform!")
@@ -20,6 +23,10 @@ import robomimic.utils.torch_utils as TorchUtils
 
 from xarm7_env import XArm7Env
 
+is_new_demo = True
+demo_count = 0
+
+execution_time = {}
 
 def main():
     """Run a trained policy from robomimic with ufactory xarm7 environment."""
@@ -48,14 +55,40 @@ def main():
             break
         print("obs:", obs)
         print("info:", info)
+
+
     while not rospy.is_shutdown():
+        if is_new_demo:
+            user_input = input("Enter to continue demo: Any key")
+            is_new_demo = False
+            demo_count = demo_count + 1
+            t_start = time.monotonic()
         # compute actions
         actions = policy(obs)
-        print("actions:", actions)
-        print("obs: ", obs)
+      
         # apply actions
         obs, reward, terminated, truncated, info= env.step(actions)
+        dones = np.array([info["is_success"]])
+        
+        if dones[0] == True:
+            execution_time[f"demo_{demo_count}_min"] = (time.monotonic() - t_start) / 60
+            is_new_demo = True
+            
+            log_dir_exec_time = os.path.join("./logs/robomimic", args_cli.task, 'agent_execution_time.json')
+            with open(log_dir_exec_time, 'w') as json_file:
+                json.dump(execution_time, json_file)
 
+            # reset env to start second evaluation
+            max_executions = 300
+            counter = 0
+            while counter < max_executions:
+                print ("resseting>>>")
+                env.step(action=actions)
+                counter += 1
+                if counter >= max_executions:
+                    info["is_success"] = False
+                    break
+                
         rate.sleep()
 
     env.close()
