e1f8c8341bc6c2cdb169d16454c30de7178a4177
AmanuelErgogo <amanuel.ergogo@gmail.com>
2023-12-12 21:39:50 +0100

    exec_time_script_modified

--- execution_time_result.py
+++ execution_time_result.py
@@ -2,14 +2,19 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Replace file paths with the correct paths
+passthrough_file_path = 'logs/robomimic/XArm7Env-v1/passthrough_execution_time.json'
+teleop_file_path = 'logs/robomimic/XArm7Env-v1/teleop_execution_time.json'
+pure_agent_file_path = 'logs/robomimic/XArm7Env-v1/pure_agent_execution_time.json'
+
 # Read JSON files
-with open('passthrough_execution_time.json', 'r') as file:
+with open(passthrough_file_path, 'r') as file:
     passthrough_data = json.load(file)
 
-with open('teleop_execution_time.json', 'r') as file:
+with open(teleop_file_path, 'r') as file:
     teleop_data = json.load(file)
 
-with open('pure_agent_execution_time.json', 'r') as file:
+with open(pure_agent_file_path, 'r') as file:
     pure_agent_data = json.load(file)
 
 # Extract execution time values
@@ -31,12 +36,19 @@ box_colors = ['lightblue', 'lightgreen', 'lightcoral']
 
 # Plot box plot with custom colors and grid
 plt.boxplot([passthrough_execution_times, teleop_execution_times, pure_agent_execution_times],
-            labels=['Passthrough', 'Teleop', 'Pure Agent'], patch_artist=True,
-            boxprops=dict(facecolor=box_colors), medianprops=dict(color='black'))
+            labels=['Passthrough', 'Teleop', 'Pure Agent'], patch_artist=True)
 
-for box, color in zip(plt.boxplot([passthrough_execution_times, teleop_execution_times, pure_agent_execution_times],
-                                  labels=['Passthrough', 'Teleop', 'Pure Agent'], patch_artist=True)['boxes'], box_colors):
+# Set facecolor for each box
+for i, (box, color) in enumerate(zip(plt.boxplot([passthrough_execution_times, teleop_execution_times, pure_agent_execution_times],
+                                  labels=['Passthrough', 'Teleop', 'Pure Agent'], patch_artist=True)['boxes'], box_colors)):
     box.set_facecolor(color)
+    # Add legend entries for each box
+    if i == 0:
+        plt.scatter(0, 0, color=color, label='Passthrough')
+    elif i == 1:
+        plt.scatter(0, 0, color=color, label='Teleop')
+    else:
+        plt.scatter(0, 0, color=color, label='Pure Agent')
 
 plt.ylabel('Execution Time (seconds)')
 plt.title('Box Plot of Execution Time')
@@ -51,5 +63,8 @@ print('Teleop Standard Deviation:', teleop_std_dev, 'seconds')
 
 print('\nPure Agent Mean Execution Time:', pure_agent_mean, 'seconds')
 print('Pure Agent Standard Deviation:', pure_agent_std_dev, 'seconds')
+
+# Add legend
+plt.legend()
 
 plt.show()
